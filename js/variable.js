b256e5c8557d5a80e2261e51c84abf8e0fa4305e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-28 10:47:37 +0900

    update

--- js/variable.js
+++ js/variable.js
@@ -7,3 +7,10 @@ var price = 100
 var discount = -100
 
 console.log(amount, price, discount)
+
+var average = 0.333
+var speed = 45.4
+
+console.log("Avg:" + average)
+console.log("Speed:" + speed)
+
