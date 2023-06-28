a1ab97f36a309449932770ec0b52598bbe069feb
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-28 11:09:05 +0900

    update

--- js/calculate.js
+++ js/calculate.js
@@ -1,14 +1,24 @@
-console.log("--- 二項演算 ---")
+console.log("--- 二項演算 ---");
 
 var hp = 5;
 hp = hp + 3;
-console.log(hp)
+console.log(hp);
 
-hp = hp - 2
-console.log(hp)
+hp = hp - 2;
+console.log(hp);
 
-hp = hp * 2
-console.log(hp)
+hp = hp * 2;
+console.log(hp);
 
-hp = hp / 3
-console.log(hp)
+hp = hp / 3;
+console.log(hp);
+
+console.log("--- 単項演算 ---");
+hp = 5;
+hp ++;
+console.log(hp);
+
+hp = 5;
+hp --
+console.log(hp);
+
