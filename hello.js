abc93e26bb26bc5fb199c137a37d4ea63eaef55e
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-06-28 10:19:52 +0900

    update

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
 console.log("Hello, YSE!!!")
+
+// コメント
+// Ctrl + / (Windows)
+// Cmd + / (Mac)
+
+// 複数行のコメント
+/**
+ * @param string name
+ * @return void
+ */
+
+// Formatter
+// Shift + Alt + F
