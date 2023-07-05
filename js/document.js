a12482ed4051c86fe2e11c91a66421a63e9762a1
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-05 10:09:06 +0900

    add outputP method

--- js/document.js
+++ js/document.js
@@ -1,4 +1,13 @@
+const outputP = function (message) {
+    var tag = "<p>" + message + "</p>"
+    document.write(tag)
+}
+
 document.write("<h1>プロフィール</h1>")
 document.write("<h2>名前</h2>")
 document.write("<p>YSE</p>")
 document.write("<h2>趣味</h2>")
+
+outputP("旅行")
+outputP("ゲーム")
+outputP("スポーツ観戦")
