808a6e514431a27f5d0116eecd9853e757aefe80
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-05 10:53:31 +0900

    update

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,8 @@
+//コンテンツ取得
 var priceElement = document.getElementById("price")
 var price = priceElement.innerText
 console.log(price)
+
+//コンテンツ更新
+var messageElement = document.getElementById("message")
+messageElement.innerText = "いらっしゃい"
