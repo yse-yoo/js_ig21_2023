cc71e29b4358c75c1e84982f5a56058c3c46388c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-05 11:17:04 +0900

    update

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,7 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 //コンテンツ取得
 var priceElement = document.getElementById("price")
 var price = priceElement.innerText
@@ -7,3 +11,13 @@ console.log(price)
 var messageElement = document.getElementById("message")
 // messageElement.innerText = "<p>いらっしゃい</p>"
 messageElement.innerHTML = "<p>いらっしゃい</p>"
+
+//タイトル「サイコロ」を表示
+var titleElement = document.getElementById("title")
+titleElement.innerHTML = "サイコロ"
+
+//サイコロの目をランダムで表示
+var number = getRandomInt(1, 6)
+console.log(number)
+var resultElement = document.getElementById("result")
+resultElement.innerHTML = number
