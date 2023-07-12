437c65e374483da056096268112b11e30617d2d1
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-12 11:10:16 +0900

    update

--- js/condition.js
+++ js/condition.js
@@ -1,3 +1,12 @@
 var message = ""
 var price = 200
 var money = 100
+
+if (price <= 0) {
+   message = "価格が間違っています" 
+} else if (moeny >= price) {
+   message = "購入できます" 
+} else {
+   message = "お金がたりません" 
+}
+console.log(message)
