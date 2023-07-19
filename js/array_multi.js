17783aba629fe18ee385159e414e6599af005c87
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-19 11:23:09 +0900

    update

--- js/array_multi.js
+++ js/array_multi.js
@@ -13,3 +13,32 @@ console.log(character.level)
 
 character.weapon = "ブロンズナイフ"
 console.log(character)
+
+//多次元配列
+var rows = [
+    [90, 78, 82,], //0
+    [62, 70, 68,], //1
+    [68, 88, 72,], //2
+]
+
+console.log(rows[1][1])
+console.log(rows[2][1])
+
+//多次元配列＆オブジェクト
+var users = [
+    {
+        id: 1,
+        name: "User1",
+        email: "user1@test.com",
+    },
+    {
+        id: 2,
+        name: "User2",
+        email: "user2@test.com",
+    },
+    {
+        id: 3,
+        name: "User3",
+        email: "user3@test.com",
+    },
+]
