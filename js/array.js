bc537a3709f474b3f97c8254acf892b259e3ac37
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-19 09:20:50 +0900

    add array

---
+++ js/array.js
@@ -0,0 +1,14 @@
+var drinks = ["コーヒー", "紅茶", "ほうじ茶"]
+console.log(drinks)
+
+var selectDrink = drinks[1]
+console.log(selectDrink)
+
+// 個数
+var count = drinks.length
+console.log(count)
+
+// push()
+console.log("--- push() ---")
+drinks.push("炭酸水")
+console.log(drinks)
