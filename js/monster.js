9fb22b4af934682209417914a33c16a52e080d55
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-19 10:01:07 +0900

    update

--- js/monster.js
+++ js/monster.js
@@ -1,1 +1,16 @@
+//parent（親要素）
 var stage = document.getElementById("stage")
+
+//child（子要素）
+var monsterElement1 = document.createElement("p")
+monsterElement1.innerHTML = "スライム"
+
+//parent に追加
+stage.appendChild(monsterElement1)
+
+var monsterElement2 = document.createElement("p")
+monsterElement2.innerHTML = "キメラ"
+stage.appendChild(monsterElement2)
+
+//childの削除
+stage.removeChild(monsterElement1)
