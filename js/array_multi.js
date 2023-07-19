c1d0c05a993ea74dcfaf8b6d89c95bd917b04e4a
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-19 11:07:34 +0900

    update

---
+++ js/array_multi.js
@@ -0,0 +1,12 @@
+var character = {
+    id: 1,
+    name: "アリス",
+    job: "wizard",
+    level: 1,
+    hp: 15,
+    mp: 10,
+    exp: 0,
+}
+console.log(character)
+console.log(character.name)
+console.log(character.level)
