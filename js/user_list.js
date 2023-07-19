816afb7e3568219f17ee4a86f8597e475bbd69f9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-07-19 11:50:56 +0900

    update

--- js/user_list.js
+++ js/user_list.js
@@ -1,3 +1,13 @@
+//Object
+var user = {}
+//Array
+var users = []
+
 function regist() {
     console.log("Regist!!!")
+    user.name = document.getElementById('user_name').value
+    user.email = document.getElementById('email').value
+    user.password = document.getElementById('password').value
+    console.log(user)
 }
+
