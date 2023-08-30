8f9ddb135eadefae40b049b18b12fa9c3b3c1bb9
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-30 09:30:44 +0900

    update

---
+++ user_list/js/user_list2.js
@@ -0,0 +1,57 @@
+var users = [
+    { id: 1, name: 'User1', email: 'user1@example.com', password: 'xxxxxx' },
+    { id: 2, name: 'User2', email: 'user2@example.com', password: 'xxxxxx' },
+    { id: 3, name: 'User3', email: 'user3@example.com', password: 'xxxxxx' },
+];
+
+function createTableRow(user) {
+    const row = document.createElement("tr");
+    const userData = { ...user }; // 元のオブジェクトを変更しないようにコピーを作成
+
+    // パスワードを削除
+    delete userData.password;
+
+    Object.values(userData).forEach(value => {
+        const cell = document.createElement("td");
+        cell.textContent = value;
+        row.appendChild(cell);
+    });
+    return row;
+}
+
+function createTable() {
+    const tableBody = document.getElementById("user_table").getElementsByTagName("tbody")[0];
+    tableBody.innerHTML = ""; // テーブルの内容を一度クリア
+
+    // 初期データの表示
+    users.forEach(user => {
+        tableBody.appendChild(createTableRow(user));
+    });
+}
+
+function regist() {
+    // フォームの値を取得
+    const name = document.getElementById("user_name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    // 入力された値を連想配列（オブジェクト）として追加
+    const newUser = {
+        id: users.length + 1,
+        name: name,
+        email: email,
+        password: password
+    };
+    users.push(newUser);
+
+    // フォームをリセット
+    document.getElementById("user_name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("password").value = "";
+
+    // テーブルを更新
+    createTable();
+}
+
+// 初期テーブルを表示
+createTable();
