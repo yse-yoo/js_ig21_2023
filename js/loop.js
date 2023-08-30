0457b7fd1e0676302af739dcbb6bddc5f6168a59
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-30 11:00:01 +0900

    add callback

--- js/loop.js
+++ js/loop.js
@@ -27,3 +27,26 @@ for (var index = 0; index < drinks.length; index++) {
     const drink = drinks[index];
     console.log(drink);
 }
+
+//コールバック関数
+
+function processArray(values, callback) {
+    //イテレータブルなデータを繰り返し
+    for (const value in values) {
+        //コールバック関数に引数を渡して実行
+        callback(value)
+    }
+}
+
+const numbers = [1, 2, 3, 4, 5]
+
+function calculate(value) {
+    var answer = value * 2 
+    console.log(answer)
+}
+processArray(numbers, calculate)
+
+
+
+
+
