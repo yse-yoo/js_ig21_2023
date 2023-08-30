75e75bf54fe6cecff7d5cc421250142417406204
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-08-30 10:28:56 +0900

    update

--- js/loop.js
+++ js/loop.js
@@ -5,9 +5,25 @@ const person = {
 }
 
 // for-in
+console.log("--- for-in ---")
 for (const key in person) {
     if (person.hasOwnProperty(key)) {
         const value = person[key];
         console.log(value)
     }
+}
+
+const drinks = ["コーヒー", "紅茶", "ほうじ茶"]
+
+// for-of
+console.log("--- for-of ---")
+for (const value of drinks) {
+   console.log(value) 
+}
+
+// fori
+console.log("--- for ---")
+for (var index = 0; index < drinks.length; index++) {
+    const drink = drinks[index];
+    console.log(drink);
 }
