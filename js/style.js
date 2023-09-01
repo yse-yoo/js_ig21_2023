f35585e6444b7c16616d2cf7d3d2007ec09ff87f
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-01 14:02:15 +0900

    update

--- js/style.js
+++ js/style.js
@@ -2,8 +2,17 @@ const messageElement = document.getElementById('message')
 
 function add() {
     messageElement.classList.add('active')
+    // messageElement.className = "message active"
 }
 
 function remove() {
     messageElement.classList.remove('active')
+}
+
+function toggle() {
+    messageElement.classList.toggle('active')
+}
+
+function replace() {
+    messageElement.classList.replace('message', 'frame')
 }
