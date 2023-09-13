8fee761710c804cc4d21b4a79704c3487632e926
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-13 10:47:10 +0900

    update

--- js/group.js
+++ js/group.js
@@ -1,6 +1,14 @@
 var cities = document.getElementsByClassName("city")
+// HTML Collection
 console.log(cities)
 
+// HTML Collection では for-in は使わない
+console.log('--- for in ---')
 for (const index in cities) {
     console.log(cities[index].innerHTML)
+}
+
+console.log('--- for of ---')
+for (const city of cities) {
+    console.log(city.innerHTML)
 }
