bcfb9544a1f5aed12f7463177aa54381997527e4
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-13 10:51:11 +0900

    update

--- js/group.js
+++ js/group.js
@@ -8,6 +8,11 @@ for (const index in cities) {
     console.log(cities[index].innerHTML)
 }
 
+// Array.from() で配列に変換
+for (const index in Array.from(cities)) {
+    console.log(cities[index].innerHTML)
+}
+
 console.log('--- for of ---')
 for (const city of cities) {
     console.log(city.innerHTML)
