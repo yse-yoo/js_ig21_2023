0712f4cdf0d88ac44cc9544839648873bafe8f7c
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-20 09:36:54 +0900

    update

--- js/selector.js
+++ js/selector.js
@@ -1,6 +1,20 @@
 var h2 = document.querySelector('h2')
 console.log(h2)
 
+// var h2 = document.getElementsByTagName('h2')
 var h2 = document.querySelectorAll('h2')
 console.log(h2[0])
 console.log(h2[1])
+
+// var cityTitle = document.getElementById('city-title')
+var cityTitle = document.querySelector('#city-title')
+console.log(cityTitle)
+
+var cities = document.getElementsByClassName('city')
+console.log(cities)
+var cities = document.querySelectorAll('.city')
+console.log(cities)
+
+for (const city of cities) {
+   console.log(city.innerHTML) 
+}
