42d809a40e21db62a968f06f9e191e452fc581af
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-20 10:10:07 +0900

    update

---
+++ js/listener.js
@@ -0,0 +1,8 @@
+var calculateBtn = document.getElementById('calculate-btn')
+// click event 登録
+calculateBtn.addEventListener('click', function() {
+    var price = document.getElementById('price').value   
+    var quantity = document.getElementById('quantity').value   
+    console.log(price, quantity)
+
+})
