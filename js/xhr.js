76176298c6fae7c4da6b8edbbe356e51bd6e92aa
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-09-27 10:13:45 +0900

    update

--- js/xhr.js
+++ js/xhr.js
@@ -1,9 +1,21 @@
 // APIのURL生成
 function getApiURL() {
-    const currentURL = location.href
+    const currentURL = location.href;
     const fileName = currentURL.substring(currentURL.lastIndexOf('/') + 1);
     const baseURL = currentURL.replace(fileName, '');
     return baseURL + 'data/persons.json';
 }
 const API_URL = getApiURL();
-console.log(API_URL)
+console.log(API_URL);
+
+// インスタンス生成
+const xhr = new XMLHttpRequest();
+//　APIに非同期通信(GET)
+xhr.open("GET", API_URL, true)
+// リクエスト後の処理
+xhr.onload = function () {
+
+}
+
+
+
